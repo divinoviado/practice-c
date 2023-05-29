9511565a17c7681c87eab2802c75eef39a2562d2
Divino Viado <2018dvinoviado@gmail.com>
2023-05-30 07:09:45 +0800

    comment & data-type

---
+++ comment&data-type/comment.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+    printf("How to Comments\n\n");
+    printf("//single-line comment\n");
+    printf("/*multi-line comment*/\n\n");
+
+//create variables
+int myNum = 5;
+float myFloatNum = 5.55;
+char myChar = 'e';
+
+//print variables
+printf("%d\n", myNum);
+printf("%f\n", myFloatNum);
+printf("%c\n\n", myChar);
+
+    return(0);
+}
